91ac9fd4037bdddfea0b2fce06e36f6d77fe250b
Filipe Fortes <filipe@fortes.com>
2010-02-13 18:52:29 -0500

    Don't hyphenate within code blocks; add more dictionaries

--- hyphenate_html.py
+++ hyphenate_html.py
@@ -13,6 +13,13 @@ def hyphenate_html(html, language='en-us', hyphenator=None):
 
     >>> hyphenate_html('<p>It is <em>beautiful</em> outside today!</p>')
     u'<p>It is <em>beau&shy;ti&shy;ful</em> out&shy;side today!</p>'
+
+    >>> hyphenate_html('O paralelepipedo atrevessou a rua', 'pt-br')
+    u'O &shy;pa&shy;ra&shy;le&shy;le&shy;pi&shy;pe&shy;do a&shy;tre&shy;ves&shy;sou a &shy;rua'
+
+    Content inside <code>, <tt>, and <pre> blocks is not hyphenated
+    >>> hyphenate_html('Document: <code>document + page_status</code>')
+    u'Doc&shy;u&shy;ment: <code>document + page_status</code>'
     """
     # Load hyphenator if one is not provided
     if not hyphenator:
@@ -32,29 +39,53 @@ ELEMENT_BLACKLIST = [
 ]
 def hyphenate_element(node, hyphenator):
     """
-    Hyphenate the text within an element
+    Hyphenate the text within an element, returning the hyphenated version
     """
-    #if node != None or (node.has_attr('name') and node.name in ELEMENT_BLACKLIST):
-        ## Don't modify text within blacklisted elements
-        #print 'backing out'
-        #return
-
     if isinstance(node, NavigableString):
         return NavigableString(hyphenator.inserted(node, SOFT_HYPHEN))
-    elif node.contents:
+    elif node.contents and node.name not in ELEMENT_BLACKLIST:
         for i, c in enumerate(node.contents):
             node.contents[i] = hyphenate_element(c, hyphenator)
-        return node
+
+    return node
 
 DICTIONARIES = {
-    'en-us': 'hyph_en_US'
+    'cs-cz': 'hyph_cs_CZ',
+    'da-dk': 'hyph_da_DK',
+    'de-ch': 'hyph_de_CH',
+    'de-de': 'hyph_de_DE',
+    'el-gr': 'hyph_el_GR',
+    'en-ca': 'hyph_en_CA',
+    'en-gb': 'hyph_en_GB',
+    'en-us': 'hyph_en_US',
+    'es-es': 'hyph_es_ES',
+    'fi-fi': 'hyph_fi_FI',
+    'ga-ie': 'hyph_ga_IE',
+    'hu-hu': 'hyph_hu_HU',
+    'ia': 'hyph_ia',
+    'id-id': 'hyph_id_ID',
+    'is-is': 'hyph_is_IS',
+    'it-it': 'hyph_it_IT',
+    'lt-lt': 'hyph_lt_LT',
+    'nl-nl': 'hyph_nl_NL',
+    'pl-pl': 'hyph_pl_PL',
+    'pt-br': 'hyph_pt_BR',
+    'pt-pt': 'hyph_pt_PT',
+    'ro-ro': 'hyph_ro_RO',
+    'ru-ru': 'hyph_ru_RU',
+    'sh': 'hyph_sh',
+    'sk-sk': 'hyph_sk_SK',
+    'sl-si': 'hyph_sl_SI',
+    'sr': 'hyph_sr',
+    'sv-se': 'hyph_sv_SE',
+    'uk-ua': 'hyph_uk_UA'
 }
 def get_hyphenator_for_language(language):
     """
     Create a Hyphenator for the given language. Uses English if the
     language is not found.
 
-    >>> get_hyphenator_for_language('en-US') #doctest: +ELLIPSIS
+    >>> get_hyphenator_for_language('ru-ru') #doctest: +ELLIPSIS
     <lib.hyphenator.Hyphenator object at ...
     """
     language = language.lower()
